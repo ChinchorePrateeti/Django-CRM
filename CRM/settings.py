0eec59e5fd380d4756967e996702720f6e354031
prateeti chinchore <ptt.chinchore@gmail.com>
2023-04-09 02:30:24 +0530

    second  commit

--- CRM/settings.py
+++ CRM/settings.py
@@ -1,14 +1,18 @@
 from pathlib import Path
 
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+from dotenv import load_dotenv
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ku_gf(l86$w1uw4(cwnyp1*!ool^08-el)y+^tzo4eh285es-m'
+
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
